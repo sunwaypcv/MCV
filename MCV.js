30396597fdbb5f6c04035d9e8011c4cb65dbe601
sunwaypcv <143777999+sunwaypcv@users.noreply.github.com>
2023-09-04 17:43:00 +0800

    Restructure

--- MCV.js
+++ MCV.js
@@ -1,6 +1,51 @@
-srcs = ["./Assets/submitted/1.png","./Assets/submitted/2.jpg","./Assets/submitted/3.jpg","./Assets/submitted/4.jpg","./Assets/submitted/5.jpg","./Assets/submitted/6.jpg","./Assets/submitted/7.jpg","./Assets/submitted/8.jpg","./Assets/submitted/9.jpg","./Assets/submitted/10.jpg","./Assets/submitted/11.jpg","./Assets/submitted/12.jpg","./Assets/submitted/13.jpg","./Assets/submitted/14.jpg","./Assets/submitted/15.jpg","./Assets/submitted/16.png"]
-titles = ["DID HE WIN OR LOSE?","Hope","Peace in the Sky","Moonshot","Golden Hour","Neighbour's Little Flower","Food for Mental Health","Crepuscule","The Blue Sky Walk","The Peaceful Beach","Dusk's Rose","A Rose Swaying In A Sea of Flames","Life goes on","Love yourself","Tide","从万丈深渊 自己治愈自己 (Saving myself from the abyss)"]
-authors = ["SCOOB DAUG","Yoong Hui Hui","Lily C","Anonymous","Low Wei Lun","YüJun :)","Low Wei Lun","Sisyphus7","Low Wei Lun","Anonymous","YY","YüJun :)","Isuann L.","Spring breeze","Anonymous","Liu Wei Zi"]
+srcs = ["./Assets/submitted/1.png","./Assets/submitted/2.jpg","./Assets/submitted/3.jpg","./Assets/submitted/4.jpg","./Assets/submitted/5.jpg","./Assets/submitted/6.jpg","./Assets/submitted/7.jpg","./Assets/submitted/8.jpg","./Assets/submitted/9.jpg","./Assets/submitted/10.jpg","./Assets/submitted/11.jpg","./Assets/submitted/12.jpg","./Assets/submitted/13.jpg","./Assets/submitted/14.jpg","./Assets/submitted/15.jpg","./Assets/submitted/16.png","./Assets/submitted/EmotionsSeries1 AJ.png","./Assets/submitted/EmotionsSeries2 AJ.png","./Assets/submitted/EmotionsSeries5 AJ.png","./Assets/submitted/EmotionsSeries8 AJ.png"]
+titles = ["DID HE WIN OR LOSE?","Hope","Peace in the Sky","Moonshot","Golden Hour","Neighbour's Little Flower","Food for Mental Health","Crepuscule","The Blue Sky Walk","The Peaceful Beach","Dusk's Rose","A Rose Swaying In A Sea of Flames","Life goes on","Love yourself","Tide","从万丈深渊 自己治愈自己 (Saving myself from the abyss)","Emotions Series 1","Emotions Series 2","Emotions Series 5","Emotions Series 8"]
+authors = ["SCOOB DAUG","Yoong Hui Hui","Lily C","Anonymous","Low Wei Lun","YüJun :)","Low Wei Lun","Sisyphus7","Low Wei Lun","Anonymous","YY","YüJun :)","Isuann L.","Spring breeze","Anonymous","Liu Wei Zi","artjamila","artjamila","artjamila","artjamila"]
+des=[["1. It helps me release stress by showcasing the absurdity of unhinged confidence and how it can affect somebody who overestimates themselves as too much of anything (in this case , confidence) can be a bad thing as the dose makes the poison.","",
+    "2. It is special to me as it represents what happens when you take confidence too far as too much confidence leads to bigotry and ignorance.","",
+    "3. I have learned that confidence is a double edge sword which can be your ally at times and your enemy at times. Confidence is something that is abstract that can't be seen. I have learned to take everything with a pinch of salt and not assume anything that appears easy  is actually easy as nothing is truly easy because the perception of something being easy can often lead you to overlooking the simplest problems.","",
+    "4.Be cautious and never naive. Nothing comes easy in life without a price to pay."],
+    ["i love looking at the sky at any time of day. it’s so beautiful that any stress i’m feeling can melt away in that moment. this particular shot was mesmerising to me as it looked like a ray of hope shining through the haze of clouds. it fills me with hope and determination that despite any hardships, good things will shine through if you just hold onto your hopefulness."],
+    ["The angle this photo was taken is towards the sky. It helps make me calm since I can imagine laying on the grass, looking and listening to the wind and trees swaying. It incites peace and tranquility. It is meaningful to me because we are often surrounded by concrete buildings; when was the last time you took a moment to admire the sky? I have learned that nature can calm us down, and for better health, we can surround ourselves with nature. #letusprotectmothernature #reduceplasticwaste"],
+    ["Moonshot","- used to refer to a plan or aim to do something that seems almost impossible,","UNLESS YOU DARE TO DREAM.","It is a dream that changes everything. It's bigger than you could ever imagine, but something you just can't ignore. It's a feeling deep in your gut."],
+    ["Sometimes I just look at the sunset in the evening, and it is a very beautiful and calming sight of nature. It's ok to fall and fail, life still has to go on. Keep your chin up and keep moving forward, you will make it one day. Looking at the sunset makes me look at the bright side of everything in life, even something that seems to be bad can be good in other ways. Don't lose your smile just because one bad incident happened because there is light at the end of the tunnel. Don't cry because it's over, smile because it happened."],
+    ["A small treat of discovering a beautiful little flower from my neighbour's garden on one of my weekends. Taking photographs of showing its beauty cherished my day ^^"],
+    ["Having a gathering while enjoying your favourite food to let all your feelings out is good for your mental health. Food unites everyone regardless of culture, food makes us happy. Being in this place is special to me because I can feel the warmth and energy in the crowd as they gather with friends and loved ones while enjoying good music. Sometimes it is ok to feel burned out, remember to go out, have fun and relax, and don't be too hard on yourself. Learn to enjoy the little things in life, life is too short for us to worry. Just follow the flow and don’t think too much, enjoy the food and company like the one here."],
+    ["The perpetual turning of the Ferris Wheel of life, encapsulates us in the eternal torture of repetition with the yearning of signs for metamorphosis. Scavenging for stimulants, yet being unconsciously desensitized towards it, morphing into a punishment by Dopamine. Though the emancipation brought forward by Endorphins is obvious, even trivial, who if any would like to embark on such a journey? One must imagine Sisyphus happy, One must imagine oneself, free. Though the ephemeral sunset, and so is our gratification and anguish, one must remember, of the good thing, that is scattered yet omnipresent."],
+    ["Walking at night is therapeutic, the cold breeze is very relaxing. As I walk through the park I get to relax my mind and be immersed in nature. I have learned that life is not always straight and rosy but full of trials and challenges, and it is up to us to make the most out of it to become a better person and to learn from each experience. Don't be afraid to try new things because life gets boring when you stay within the limits of what you already know."],
+    ["Everytime I’m at the beach, it’s like my stress is forgotten. I feel peace whenever I'm at the beach, it gives me a calming vibe that everything will turn out fine. Listening to the waves of the ocean is like music to my ears."],
+    [`My friend shot this photo and sent it to me with the message: “One day you will definitely shine beautifully in places that you did not expect. Embracing Life's Transitions. Just as the rose finds its beauty under the fading light, remember that even in moments of change and uncertainty, there's a quiet strength within you. Each transition is a step toward a new dawn, where your resilience blooms."`],
+    [`Throughout our lives, people must endure numerous trials, experience gatherings and partings, tread through perilous paths and fiery trials, and constantly engage in a battle against an intangible self (whether it be their past self or an imagined self). Yet, even amidst the desert, you can still become that rose. This rose doesn't necessarily have to be red; it could also be blue, green, yellow, white, purple, or even transparent. You are yourself, and you are also your own hero.`],
+    [`The lockdowns in 2020-21 were a tumultuous time full of uncertainty. On the bright side, I rediscovered drawing as a creative outlet and a way to avoid screen time; to this date, drawing still brings me much peace. Why a semicolon? A semicolon represents hope and continuation of life for those who are struggling with mental health. According to Project Semicolon, "a semicolon is used when an author could've chosen to end their sentence but chose not to. The author is you, and the sentence is your life." The flowers growing from the semicolon represent new life. (Another reason is that I enjoy drawing plants more than anything else.) This is a visual reminder that this, too, shall pass. Remember that you're not alone. Reach out to those you trust.`],
+    [`Find beauty in things around you.`],
+    [`It was very relaxing to draw this picture because it made me forget about other things that day and I haven't used crayons for years. I had a lot of fun creating this piece of artwork with a friend and I focused more on the process rather than the result. I think it's important to enjoy drawing and be present, even if the finished product is not as expected.`],
+    [`My feelings needed an outlet, they guided my painting. Through painting, I saved myself from the abyss, stroke by stroke, bit by bit, inch by inch, I got out. `],
+    [`Autistic people feel a range of emotions from happiness, shock, sadness and many more. However, they tend to feel it in a deeper, more intense way than we do. They are more sensitive and aware of their environment due to their deep connection to their senses, which shows that they feel the same things we feel, but with more intensity– making them unique but never less.`],
+    [`Autistic people feel a range of emotions from happiness, shock, sadness and many more. However, they tend to feel it in a deeper, more intense way than we do. They are more sensitive and aware of their environment due to their deep connection to their senses, which shows that they feel the same things we feel, but with more intensity– making them unique but never less.`],
+    [`Reality can be harsh. The Covid-19 pandemic is like a layer of oil in the sea of our lives that keeps us from breathing. This painting is about those who are stuck at the bottom. They face challenges that pile up from unemployment, the distancing of relationships and mental health issues from the lack of contact and many more. The various expressions painted on this canvas represent a struggle of anger, shock, momentary happiness and depression. The heads are close to each other, showing how fast one's mood can change due to the hectic life in the pandemic.`],
+    [`Reality can be harsh. The Covid-19 pandemic is like a layer of oil in the sea of our lives that keeps us from breathing. This painting is about those who are stuck at the bottom. They face challenges that pile up from unemployment, the distancing of relationships and mental health issues from the lack of contact and many more. The various expressions painted on this canvas represent a struggle of anger, shock, momentary happiness and depression. The heads are close to each other, showing how fast one's mood can change due to the hectic life in the pandemic.`]
+]
+externallinks = ["https://artjamila.com/portfolio/emotions-series-1/","https://artjamila.com/portfolio/emotions-series-2/","https://artjamila.com/portfolio/emotions-series-5/","https://artjamila.com/portfolio/emotions-series-8/"]
+
+contentcontainer = document.getElementById("contentcontainer");
+link = document.getElementById("displaylink");
+for(var x=0;x<srcs.length;x++){
+    container=document.createElement("div");
+    container.id="content"+(x+1).toString();
+    container.classList.add("content",(x+1).toString());
+    image=document.createElement("img");
+    image.classList.add("piece",(x+1).toString());
+    title=document.createElement("h4");
+    title.classList.add((x+1).toString());
+    descriptionswrapper=document.createElement("pre");
+    descriptions=document.createElement("p");
+    descriptionswrapper.appendChild(descriptions);
+    descriptions.classList.add((x+1).toString());
+    container.appendChild(image);
+    container.appendChild(title);
+    container.appendChild(descriptionswrapper);
+    contentcontainer.appendChild(container);
+}
 
 menuicon = document.getElementById("menu-icon")
 menudiv = document.getElementById("menu")
@@ -12,11 +57,9 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 document.getElementById("closedisplay").onclick=function(){
     document.getElementById("display").classList.remove("active");
-    // document.getElementById("display").style.display="none";
 }
 
 window.onload=function(){
-    document.getElementById("description1").style.display="block";
     if(window.innerWidth>800){proceed(1)}
 }
 
@@ -30,6 +73,34 @@ for(var x=0;x<contents.length;x++){
     titlestag[x].innerHTML=titles[x]
     authorstag[x].innerHTML="By: "+authors[x];
     pieces[x].setAttribute("src",srcs[x]);
+}
+
+
+desp=document.getElementById("displaydescriptions");
+desp.style.transform="none";
+desp.style.maxWidth="100%";
+// desp.id = "description"+(x+1).toString();
+
+function deschange(z){
+    a = document.createElement("p");
+    text = des[z]
+    for(var y=0;y<text.length;y++){
+        a.appendChild(document.createTextNode(des[z][y]));
+        a.appendChild(document.createElement("br"));
+    }
+    return a
+}
+
+function change(z){
+    desp.innerHTML='';
+    for(var q=0;q<desp.childNodes.length;q++){
+        desp.childNodes[q].innerHTML="";
+        desp.removeChild(desp.childNodes[q]);
+    }
+    while(desp.firstChild){
+        desp.removeChild(desp.firstChild);
+    }
+    desp.appendChild(deschange(z));
 }
 
 menuicon.onclick=function(){
@@ -49,10 +120,15 @@ function proceed(x){
     document.getElementById("displaypiece").setAttribute("src",srcs[x-1]);
     document.getElementById("displaytitle").innerHTML=titles[(x-1)];
     document.getElementById("displayauthor").innerHTML="By: "+authors[(x-1)];
-    for(var i=1;i<=contents.length;i++){
-        document.getElementById("description"+i).style.display="none";
+    change(x-1);
+    if(x<17){
+        link.style.display="none";
+        document.getElementsByClassName('displaydescriptions').style.top="58dvh";
+    }else{
+        link.style.display="block";
+        link.href=externallinks[x-17];
+        document.getElementsByClassName('displaydescriptions').style.top="63dvh";
     }
-    document.getElementById("description"+x.toString()).style.display="block"  
 }
 
 for(var x=0;x<contents.length;x++){
